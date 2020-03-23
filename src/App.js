83b1c39fbdab433b3e58a6ca7dbed8ea6e4d0ad0
Ekin Alçar <ekinalcar@gmail.com>
2020-03-23 19:10:04 +0100

    done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,45 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    return <div>1</div>;
+    return (
+      <div className="container">
+        <section>
+          <h1>Join our community</h1>
+          <h2 className="yellow-heading">
+            30-day, hassle-free money back guarantee
+          </h2>
+          <p className="light-black">
+            Gain access to our full library of tutorials along with expert code
+            reviews.
+            <br />
+            Perfect for any developers who are serious about honing their
+            skills.
+          </p>
+        </section>
+        <div className="section-container">
+          <section className="cyan">
+            <h3>Monthly Subscription</h3>
+            <p className="faded">
+              <span>$29</span> per month
+            </p>
+            <p>Full access for less than $1 a day</p>
+            <button type="button">Sign Up</button>
+          </section>
+          <section className="grayish-blue">
+            <h2>Why Us</h2>
+            <ul>
+              <li>Tutorials by industry experts</li>
+              <li>Peer &amp; expert code review</li>
+              <li>Coding exercises</li>
+              <li>Access to our GitHub repos</li>
+              <li>Community forum</li>
+              <li>Flashcard decks</li>
+              <li>New videos every week</li>
+            </ul>
+          </section>
+        </div>
+      </div>
+    );
   }
 }
 
